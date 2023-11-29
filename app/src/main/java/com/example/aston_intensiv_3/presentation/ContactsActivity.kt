58b2f2b6393213d9e406c0a016b3b9895d1136da
Tambosa <tambosa@gmail.com>
2023-11-29 12:30:49 +0300

    Initial commit

---
+++ app/src/main/java/com/example/aston_intensiv_3/presentation/ContactsActivity.kt
@@ -0,0 +1,22 @@
+package com.example.aston_intensiv_3.presentation
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.ViewModelProvider
+import com.example.aston_intensiv_3.databinding.ActivityContactsBinding
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class ContactsActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityContactsBinding
+    private lateinit var viewModel: ContactsViewModel
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityContactsBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        viewModel = ViewModelProvider(this)[ContactsViewModel::class.java]
+
+    }
+}
